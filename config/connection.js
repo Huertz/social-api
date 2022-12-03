3af15dff5608a62f4eac1439edfa83f2fe88b01e
Huertz <104065687+Huertz@users.noreply.github.com>
2022-12-03 16:39:23 -0600

    server.js

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-api",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
