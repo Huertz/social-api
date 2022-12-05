67af741a36248531f638fd7693492d9af3fd41e8
Huertz <104065687+Huertz@users.noreply.github.com>
2022-12-05 02:34:15 -0600

    completed models/Thought

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create reaction model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types,ObjectId,
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Schema to create though model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //todo Use a getter method to format the timestamp on query
+      //get:
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    // Use to validate reactionSchema
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+// Virtual schema 
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+mongoose.model('thought', reactionSchema)
+
+// Exports thought
+module.exports = Thought;
